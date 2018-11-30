3ac6b8bbb86618780a37f04438e4ba193980cbf2
Rodrigo Pinheiro <rpinheir@thoughtworks.com>
2018-11-29 22:42:20 -0200

    Adding a function Debug

--- scream.sh
+++ scream.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+DEBUG=1
+
+Debug() {
+    [ "$DEBUG" = 1 ] && echo -e "\033[33;1m$*\033[m"
+}
+
 TXT="screaming"
 TXT="     $TXT     "
+Debug "TXT with spaces  : [$TXT]"
 TXT=$(echo "$TXT" | tr ' ' '!')
+Debug "TXT with exclamations:   [$TXT]"
 TXT=$(echo $TXT | tr a-z A-Z)
 echo "$TXT"
